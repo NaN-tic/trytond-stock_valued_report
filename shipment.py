6caf94a4f2efc0d7761471558cc504918c58e0a2
resteve <resteve@zikzakmedia.com>
2012-07-17 18:53:35 +0200

    Add Delivery Note Valued ODT + DeliveryValued Shipment class

---
+++ shipment.py
@@ -0,0 +1,22 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
+from trytond.transaction import Transaction
+from trytond.pool import Pool
+from trytond.modules.company import CompanyReport
+
+class DeliveryValued(CompanyReport):
+    _name = 'stock.shipment.out.delivery_valued'
+
+    def parse(self, report, objects, datas, localcontext):
+        localcontext['product_name'] = lambda product_id, language: \
+                self.product_name(product_id, language)
+        return super(DeliveryValued, self).parse(report, objects, datas,
+                localcontext)
+
+    def product_name(self, product_id, language):
+        product_obj = Pool().get('product.product')
+        with Transaction().set_context(language=language):
+            return product_obj.browse(product_id).rec_name
+
+DeliveryValued()
