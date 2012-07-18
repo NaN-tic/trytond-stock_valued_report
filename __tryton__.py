175d9aa5d734dc5b0c7011ea7dfef7217f0e7c7f
resteve <resteve@zikzakmedia.com>
2012-07-18 09:31:00 +0200

    Delivery Note Report Rules

--- __tryton__.py
+++ __tryton__.py
@@ -8,9 +8,21 @@
     'author': 'Zikzakmedia',
     'email': 'zikzak@zikzakmedia.com',
     'website': 'http://www.zikzakmedia.com/',
-    'description': '''Report Stock Valued''',
-    'description_ca_ES': '''Informe albarans valorats''',
-    'description_es_ES': '''Informe albaranes valorados''',
+    'description': '''Report Stock Valued.
+In Delivery Note Report, you can add group permisions:
+- Stock
+- Stock Price 
+    ''',
+    'description_ca_ES': '''Informe albarans valorats.
+A la configuració del informe Albarà d'entrega', pot afegir els grups:
+- Estoc
+- Preus estoc
+    ''',
+    'description_es_ES': '''Informe albaranes valorados.
+A la configuración del informe Albarán de entrega, puede añadir los grupos:
+- Stock
+- Precios stock
+''',
     'depends': [
         'company',
         'stock_valued',
